aebcc1479312e5488e81b68bbc3023fc0121cb45
The TT Hacker <thetthacker@localhost.localdomain>
2019-12-16 16:01:27 +1100

    graph builder working for haversine distance

--- graph.py
+++ graph.py
@@ -1,22 +1,97 @@
+from math import radians, cos, sin, asin, sqrt
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers
+
+    return c * r
+
 class Graph:
 	def __init__(self):
 		self.nodes = []
 
+	def add_node(self, tournament):
+		edges = []
+
+		node = Node(tournament["name"], tournament["start_date"], tournament["end_date"], edges)
+
+		self.nodes.append(node)
+
+	def find_node(self, tournament):
+		for node in self.nodes:
+			if node.name == tournament["name"] and node.start_date == tournament["start_date"]:
+				return node
+
+	def add_distance_edges(self, tournaments):
+		for i, node in enumerate(self.nodes):
+			j = i + 1
+
+			for child_node in self.nodes[i + 1:]:
+				
+				distance = haversine(tournaments[i]["lon"], tournaments[i]["lat"], tournaments[j]["lon"], tournaments[j]["lat"])
+
+				node.add_edge(child_node, distance)
+
+				j += 1
+
+	def __str__(self):
+		string = ""
+
+		for node in self.nodes:
+			string += node.name + "\n"
+
+			for edge in node.edges:
+				string += "\t" + edge.node.name
+
+			string += "\n"
+
+		return string
+
 class Node:
-	def __init__(self):
-		self.name
-		self.start_date
-		self.end_data
-		self.edges
+	def __init__(self, name, start_date, end_date, edges):
+		self.name       = name
+		self.start_date = start_date
+		self.end_date   = end_date
+
+		# Only nodes with events that occur after
+		self.edges = edges
+
+	def add_edge(self, node, weight):
+		edge = Edge(node, weight)
+
+		self.edges.append(edge)
 
 class Edge:
-	def __init__(self, node1, node2, weight):
-		self.node1  = node1
-		self.node2  = node2
+	def __init__(self, node, weight):
+		self.node   = node
 		self.weight = weight
 
+"""
+Assumes tournaments are in chronological order
+"""
+
 def build_graph(tournaments):
-	pass
+
+	graph = Graph()
+
+	for tournament in tournaments:
+		graph.add_node(tournament)
+
+	graph.add_distance_edges(tournaments)
+
+	return graph
 
 if __name__ == "__main__":
-	build_graph()
+	pass
--- main.py
+++ main.py
@@ -5,18 +5,26 @@ Algorithm to calcuate the ideal tournaments someone should play
 """
 
 from filter     import *
-from graph      import *
-from calculator import *
+from graph      import build_graph
+from calculator import calculate_region_tour_num_tournaments
+
+from datetime import datetime
 
 NUM_TOURNAMENTS = 2
 
 tournaments = [
-	{"name": "Sydney Open", "lat": , "lon": , "start_date": "", "end_date": ""}
+	{"name": "Sydney Open",     "lat": -33.854980, "lon": 151.072281, "start_date": datetime(2020, 1, 1), "end_date": datetime(2020, 1, 4)},
+	{"name": "Newcastle Open",  "lat": -32.923188, "lon": 151.729923, "start_date": datetime(2020, 1, 6), "end_date": datetime(2020, 1, 10)},
+	{"name": "Wollongong Open", "lat": -34.418964, "lon": 150.886659, "start_date": datetime(2020, 1, 12), "end_date": datetime(2020, 1, 16)},
+	{"name": "Bathurst Open",   "lat": -33.407295, "lon": 149.576608, "start_date": datetime(2020, 1, 18), "end_date": datetime(2020, 1, 22)},
+	{"name": "Manly Open",      "lat": -33.796415, "lon": 151.285392, "start_date": datetime(2020, 1, 24), "end_date": datetime(2020, 1, 28)}
 ]
 
 if __name__ == "__main__":
-	graph = build_graph()
+	graph = build_graph(tournaments)
 
-	tour = calculate_region_tour_num_tournaments(graph, NUM_TOURNAMENTS)
+	print(graph)
 
-	print(tour)
+	#tour = calculate_region_tour_num_tournaments(graph, NUM_TOURNAMENTS)
+
+	#print(tour)
